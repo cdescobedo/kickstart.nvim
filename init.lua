d68f074b8485cbbf710051c6462190395eb93534
Carlos D. Escobedo <cescobedo@complecti.io>
2025-12-12 21:59:46 -0700

    fix(init.lua): show hidden files in tree-sitter

--- init.lua
+++ init.lua
@@ -426,12 +426,26 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          file_ignore_patterns = {
+            'node_modules',
+            '.git/',
+            '.venv/',
+            '__pycache__/',
+            '.pytest_cache/',
+            '.ruff_cache',
+          },
+          -- mappings = {
+          --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          -- },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            no_ignore = true,
+          },
+        },
+
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
